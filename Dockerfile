63e383a6fa317cd7d8fb8bc70585f8e408fb2890
Yaadee <ayyeekoo437@gmail.com>
2023-11-14 13:05:19 +0300

    Add all files

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+
+FROM node:16.20.2
+WORKDIR /app
+COPY package*.json ./
+RUN npm install
+COPY . .
+
+RUN npm run build
+
+ENV NODE_ENV=production
+
+EXPOSE 3000
+
+CMD ["npm", "run", "start"]
---
+++ src/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+
+            Wecome to pipeline test!
+            How about now
+            
+        </div>
+    )
+}
