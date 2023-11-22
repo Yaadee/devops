96d039f0a240480ce652564a9ae1bf237b657197
Yaadee <ayyeekoo437@gmail.com>
2023-11-22 11:30:46 +0300

    edited

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -6,6 +6,7 @@ export default function Home() {
 
             Wecome to pipeline test!
             How about now, lets test jenkins now
+            <h> edited</h>
             
         </div>
     )
